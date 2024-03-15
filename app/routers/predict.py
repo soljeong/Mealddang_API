56e2173885cb4aa3274e740fad2b26161a22bb92
DaSeul-Seo <star.dss999@gmail.com>
2024-03-15 15:53:13 +0900

    yolo-update

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -10,7 +10,7 @@ async def post_predict(file: UploadFile = File(...)):
     return await YoloService().service_yolo(file)
 
 @router.get("/download/{filename}")
-async def post_download(filename: str):
+async def get_download(filename: str):
     # filename = UUID_label_x_y_w_h_conf.jpg
     # 서버에 저장된 파일을 클라이언트에게 반환합니다.
     return service_file(filename)
