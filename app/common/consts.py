77ef30fbdb234807ff60621f31773e044a0cf62c
sol <jeongsol@outlook.com>
2024-03-18 02:39:24 +0000

    경로 변경

--- app/common/consts.py
+++ app/common/consts.py
@@ -8,10 +8,10 @@ DB_NAME = "mealddang"
 
 TODAY = datetime.now().strftime("%Y%m%d")
 DEVICE = "cpu"
-YOLO_PY_PATH = "yolov3\\detect_meal.py"
-CFG_PATH = "yolov3\\cfg\\best_md_403.cfg"
-PT_PATH = "yolov3\\weights\\best_md_403.pt"
-CLASS_PATH = "yolov3\\data\\best_md_403_cls.names"
-OUTPU_DIR = f"app\\result\\{TODAY}\\output"
-SAVE_DIR = f"app\\result\\{TODAY}\\source"
+YOLO_PY_PATH = "yolov3/detect_meal.py"
+CFG_PATH = "yolov3/cfg/best_md_403.cfg"
+PT_PATH = "yolov3/weights/best_md_403.pt"
+CLASS_PATH = "yolov3/data/best_md_403_cls.names"
+OUTPU_DIR = f"app/result/{TODAY}/output"
+SAVE_DIR = f"app/result/{TODAY}/source"
 CONF_THRES = 0.4
